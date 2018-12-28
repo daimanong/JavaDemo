bdf322294ec7ef19223acce3d84aec71796cf732
SEE YOU AGAIN <SEE YOU AGAIN@192.168.191.3>
2018-12-28 21:25:18 +0800

    演示Demo

---
+++ src/JavaDemo.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class JavaDemo {
+public static void main(String []args) {
+	Scanner scanner=new Scanner(System.in);
+	System.out.println(scanner.nextLine());
+}
+}
